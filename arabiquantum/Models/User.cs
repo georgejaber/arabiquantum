c6b6688b322c0f291c47ad594c821bf7681749ac
momen <momen.mafarjeh@students.alquds.edu>
2023-04-19 10:29:56 +0300

    user data

--- arabiquantum/Models/User.cs
+++ arabiquantum/Models/User.cs
@@ -1,6 +1,15 @@
 ﻿namespace arabiquantum.Models
 {
-    public class Class
+    public class User
+
     {
+        [key]
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime  birthData { get; set; }
+        public string  role { get; set; }
+
     }
 }
