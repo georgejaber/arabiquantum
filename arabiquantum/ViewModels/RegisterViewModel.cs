d22a8dbec133b146f5b902b398204adc8d07e184
george <jaber.george2@gmail.com>
2023-04-22 16:50:39 +0300

    Register Feature adeed

---
+++ arabiquantum/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace arabiquantum.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Display(Name = "Username")]
+        [Required(ErrorMessage ="Username Is Required")]
+        public string Username { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Email Is Required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Password Is Required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "ConfirmPassword")]
+        [Required(ErrorMessage = "ConfirmPassword Is Required")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
